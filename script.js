91b24b0dd8a8b8ecad5f6cb67d012796469cce01
Prokhorenko Elizaveta <101064701+Liz16k@users.noreply.github.com>
2022-05-28 12:02:06 +0300

    fix: change approach

--- script.js
+++ script.js
@@ -1,59 +1,100 @@
 $ = selector => document.querySelector(selector)
 $$ = selector => [...document.querySelectorAll(selector)]
+const format = (time) => (time > 9) ? time : '0' + time
 
-const msg = $('.msg')
+const templateTodo = (task) => {
+  const checkedAttr = task.isChecked ? 'checked' : ''
+  const classChecked = task.isChecked ? 'done' : ''
+  return `
+    <div class="task-body ${classChecked}" id=${task.id}>
+      <input class="form-check-input" id="lg" type="checkbox" ${checkedAttr}>
+      <input class="form-control" id="text" type="text" value="${task.content}" disabled>
+      <div class="form-control" id="date">${task.createdAt}</div>
+      <button class="btn btn-primary delete" type="button"><span class="material-symbols-outlined delete">delete_forever</span></button>
+    </div>`
+}
 
-let arrayOfTasks = []
+let todos = JSON.parse(localStorage.getItem('todos'))
+
+let data = (todos) ? [...todos] : []
+let todoJSON = JSON.stringify(data)
+localStorage.setItem('todos', todoJSON)
+
+let inputContent = $('#input')
 const list = $('.task-list')
 const inputForm = $('.menu-form')
-let inputText = $('#input')
+const delAll = $('#del-all')
 
-inputForm.addEventListener('submit', function (e) {
+render()
+
+inputForm.addEventListener('submit', handleSubmitForm)
+delAll.addEventListener('click', handleClickBtnDelAll)
+list.addEventListener('click', handleClickBtnDel)
+list.addEventListener('change', handleInputChange)
+
+function handleSubmitForm (e) {
   e.preventDefault()
-  if (inputText.value) {
-    const date = new Date()
-    const day = date.getDate()
-    const month = date.getMonth()
-    const hour = date.getHours()
-    const minute = date.getMinutes()
-    const now = `${(hour > 9) ? hour : '0' + hour}:${(minute > 9) ? minute : '0' + minute} ${day}.${(month > 9) ? month : '0' + (month + 1)}`
-
-    arrayOfTasks.push({text: inputText.value, date: now})
-    
-    const task = document.createElement('form')
-    task.classList.add('form-check')
-    list.prepend(task)
-    task.innerHTML = 
-    `<form class="form-check">
-        <div class="task-body">
-          <input class="form-check-input" id="lg" type="checkbox" name="">
-          <input class="form-control" id="text" disabled type="text" value="${arrayOfTasks.at(-1).text}">
-          <div class="form-control" id="date">${arrayOfTasks.at(-1).date}</div>
-          <button class="btn btn-primary delete" id="${arrayOfTasks.length}" type="button"><span class="material-symbols-outlined">delete_forever</span></button>
-        </div>
-      </form>`
-    
+  const content = inputContent.value
+  if (content) {    
+    const task = new Todo(content)
+    data.push(task)
     inputForm.reset()
-
+    render()
   } else alert('Enter to do...');
+}
 
-  const delAll = $('#del-all')
-  delAll.onclick = function () {
-    list.innerHTML = ''
-    arrayOfTasks.length = 0
+function handleInputChange (e) {
+  if (e.target.classList.contains('form-check-input')) {
+    const task = e.target.closest('.task-body')
+    const id = task.id
+    data.forEach(task => {
+      if (task.id == id) {
+        task.isChecked = e.target.checked
+        render()
+      }
+    })
   }
+}
 
-  const DeleteBtns = $$('.delete')
-  DeleteBtns.forEach(task => {
-    task.onclick = function () {
-      task.parentNode.parentNode.remove(task)
-      arrayOfTasks[task.id - 1] = 'null'
-      arrayOfTasks = arrayOfTasks.filter(task => task != 'null')
-    }
-  });
+function handleClickBtnDel (e) {
+  if (e.target.classList.contains('delete')) {
+    const task = e.target.closest('.task-body')
+    const id = task.id
+    data.forEach((task, index) => {
+      if (task.id == id) {
+        data.splice(index, 1)
+        render()
+      }
+    }) 
+  }
+}
 
-  const chboxes = $$('.form-check-input')
-  chboxes.forEach(chbox => {
-    chbox.onchange = () => chbox.parentNode.parentNode.classList.toggle('done')
-  })
-})
+function handleClickBtnDelAll () {
+  data.length = 0
+  render()
+}
+
+function Todo (content) {
+  const time = new Time()
+  this.id = time.date.getTime()
+  this.content = content
+  this.createdAt = time.formatTime
+  this.isChecked = false
+}
+
+function Time () {
+  const date = new Date() 
+  this.date = date
+  this.day = date.getDate()
+  this.month = date.getMonth() + 1
+  this.hour = date.getHours()
+  this.minute = date.getMinutes()
+  this.formatTime = `${format(this.hour)}:${format(this.minute)} ${format(this.day)}.${format(this.month)}`
+}
+
+function render () {
+  list.innerHTML = ''
+  list.innerHTML = data.reduce((html, task) => templateTodo(task) + html , '')
+  todoJSON = JSON.stringify(data)
+  localStorage.setItem('todos', todoJSON)
+}
